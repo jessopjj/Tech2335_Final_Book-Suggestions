6ff30eb60b8cd1d65facadac56ef34dc847ab869
jessopjj <jjj9717@stern.nyu.edu>
2023-08-12 10:42:45 -0400

    HTML Tweak + name = main in app

--- app/book_suggestions.py
+++ app/book_suggestions.py
@@ -32,29 +32,29 @@ def suggest_random_book(books_data):
     else:
         print("Sorry, no books found for your query.")
 
+if __name__ == "__main__":
+    # Prompt the user for input
+    user_query = input("Enter a genre or title to search for: ")
 
-# Prompt the user for input
-user_query = input("Enter a genre or title to search for: ")
+    # Search for books based on user input
+    books_data = search_books(user_query)
 
-# Search for books based on user input
-books_data = search_books(user_query)
-
-# Display search results
-print("Search Results:")
-for index, book in enumerate(books_data.get('items', []), start=1):
-    volume_info = book.get('volumeInfo', {})
-    title = volume_info.get('title', 'No Title')
-    authors = volume_info.get('authors', ['Unknown Author'])
-    print(f"{index}. Title: {title}")
-    print(f"   Authors: {', '.join(authors)}")
-    print("=" * 20)
+    # Display search results
+    print("Search Results:")
+    for index, book in enumerate(books_data.get('items', []), start=1):
+        volume_info = book.get('volumeInfo', {})
+        title = volume_info.get('title', 'No Title')
+        authors = volume_info.get('authors', ['Unknown Author'])
+        print(f"{index}. Title: {title}")
+        print(f"   Authors: {', '.join(authors)}")
+        print("=" * 20)
 
 
-print("Woah! These are are a lot of books. Which one should you start with?")
+    print("Woah! These are are a lot of books. Which one should you start with?")
 
-# Ask if the user wants a random book suggestion
-random_choice = input("Do you want a random book suggestion? (ofc/no): ")
-if random_choice.lower() == 'ofc':
-    suggest_random_book(books_data)
-else:
-    print("Okay! Enjoy the list above!")
+    # Ask if the user wants a random book suggestion
+    random_choice = input("Do you want a random book suggestion? (ofc/no): ")
+    if random_choice.lower() == 'ofc':
+        suggest_random_book(books_data)
+    else:
+        print("Okay! Enjoy the list above!")
